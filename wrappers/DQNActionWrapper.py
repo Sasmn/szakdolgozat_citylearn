c0d6e74356f5cfbbaf0173370d4bdf525343ec65
sasmn <0.3sasi@gmail.com>
2024-03-16 18:43:48 +0100

    modified custom DQN wrapper to be more memory efficient

--- wrappers/DQNActionWrapper.py
+++ wrappers/DQNActionWrapper.py
@@ -3,6 +3,8 @@ from citylearn.citylearn import CityLearnEnv
 from typing import List, Mapping
 from wrappers.DiscreteActionWrapper import DiscreteActionWrapper
 import itertools
+import numpy as np
+import sys
 
 class DQNActionWrapper(ActionWrapper):
     """Action wrapper for :py:class:`citylearn.agents.q_learning.TabularQLearning` agent.
@@ -23,19 +25,51 @@ class DQNActionWrapper(ActionWrapper):
         env = DiscreteActionWrapper(env, bin_sizes=bin_sizes, default_bin_size=default_bin_size)
         super().__init__(env)
         self.env: CityLearnEnv
-        self.combinations = self.set_combinations()
+        self.options = self.get_options()
+        self.action_lengths = self.get_action_lenghts()
+        self.combinations_lenght = self.get_combinations_length()
 
     @property
     def action_space(self) -> spaces.Discrete:
         """Returns action space for discretized actions."""
-        return spaces.Discrete(len(self.combinations))
+        return spaces.Discrete(self.combinations_lenght)
     
     def action(self, action: float) -> List[int]:
         """Returns discretized action."""
-        return self.combinations[int(action)]
+        return self.get_combination(int(action))
     
-    def set_combinations(self) -> List[int]:
-        """Returns all combinations of discrete actions."""
-        options = [list(range(d.n)) for d in self.env.action_space[0]]
-        combs = [comb for comb in itertools.product(*options)]
-        return combs
+    def get_combination(self, action: int) -> List[int]:
+        """Returns a discretized action combination given an action."""
+
+        lengths = self.action_lengths
+        total_combinations = self.combinations_lenght
+
+        combination = []
+
+        for length in reversed(lengths):
+            total_combinations //= length
+            quotient, action = divmod(action, total_combinations)
+            combination.append(self.options[len(lengths) - len(combination) - 1][quotient])
+    
+        return combination
+    
+    def get_combinations_length(self) -> int:
+        """Returns the length of all combinations of discrete actions."""
+
+        lengths = [len(sublist) for sublist in self.options]
+        total_combinations = 1
+        for length in lengths:
+            total_combinations *= length
+        return total_combinations
+    
+    def get_options(self) -> List[List[int]]:
+        """Returns all possible set of discrete values for the actions."""
+
+        options = [list(np.arange(d.n, dtype=np.int8)) for d in self.env.action_space[0]]
+        return options
+    
+    def get_action_lenghts(self) -> List[int]:
+        """Returns the length of all combinations of discrete actions."""
+
+        lengths = [len(sublist) for sublist in self.options]
+        return lengths
